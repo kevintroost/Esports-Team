ea87d3c501bcc7cc59d2d5620d3297eb59123f3d
PERENSAPP <119853517+PERENSAPP@users.noreply.github.com>
2023-05-24 13:41:24 +0200

    menu fail update
    Ik kan de menu niet laten zien op de admin page

--- wordpress/wp-content/themes/esportsTheme/fuctions.php
+++ wordpress/wp-content/themes/esportsTheme/fuctions.php
@@ -1,3 +1,4 @@
+<!-- add stylesheet -->
 <?php 
 
 add_action("wp_enqueue_scripts", "customCss");
@@ -5,4 +6,14 @@ function customCss(){
     wp_enqueue_style("custom_css", get_template_directory_uri()."/style.css"); 
 }
 
-?> 
+// <!-- Showing the menu tab in the admin panel -->
+function register_my_menus() {
+    register_nav_menus(
+      array(
+        'header-menu' => __( 'Header Menu' ),
+        'extra-menu' => __( 'Extra Menu' )
+       )
+     );
+   }
+   add_action( 'init', 'register_my_menus' );
+
