7d7fba4c1618ad2bd83752ab5fb42775145b7511
PERENSAPP <119853517+PERENSAPP@users.noreply.github.com>
2023-06-07 13:13:52 +0200

    update content unlock

--- wordpress/wp-content/themes/esportsTheme/functions.php
+++ wordpress/wp-content/themes/esportsTheme/functions.php
@@ -1,24 +1,23 @@
 <!-- add stylesheet -->
-<?php 
+<?php
 
 add_action("wp_enqueue_scripts", "customCss");
-function customCss(){ 
-    wp_enqueue_style("custom_css", get_template_directory_uri()."/style.css"); 
+function customCss()
+{
+  wp_enqueue_style("custom_css", get_template_directory_uri() . "/style.css");
 }
 
 // <!-- Showing the menu tab in the admin panel -->
-function register_my_menus() {
-    register_nav_menus(
-      array(
-        'header-menu' => __( 'Header Menu' ),
-        'extra-menu' => __( 'Extra Menu' )
-       )
-     );
-   }
-   add_action( 'init', 'register_my_menus' );
+function register_my_menus()
+{
+  register_nav_menus(
+    array(
+      'header-menu' => __('Header Menu'),
+      'extra-menu' => __('Extra Menu')
+    )
+  );
+}
+add_action('init', 'register_my_menus');
 
 
-
-   
-
-?> 
+  ?>
--- wordpress/wp-content/themes/esportsTheme/index.php
+++ wordpress/wp-content/themes/esportsTheme/index.php
@@ -5,19 +5,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <?php wp_nav_menu(array('theme_location' => 'header-menu')); ?>
+    <title></title>
 </head>
 
 <body>
     <?php wp_head(); ?>
-    <?php wp_nav_menu(array('theme_location' => 'header-menu')); ?>
-    <?php echo get_the_title(); ?>
-       
-    <?php get_header() ?> 
-   
 
-    <?php get_footer(); ?>
+    
 
+    <div class="container"><?php echo get_the_content();?></div>
+
+    <?php wp_footer(); ?>   
 </body>
 
 </html>     
