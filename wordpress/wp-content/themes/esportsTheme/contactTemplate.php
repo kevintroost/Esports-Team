bb69e69098ef4f62264a23b2db20bdb2be391dc4
PERENSAPP <peren.sapp@gmail.com>
2023-06-16 22:58:43 +0200

    update

--- wordpress/wp-content/themes/esportsTheme/contactTemplate.php
+++ wordpress/wp-content/themes/esportsTheme/contactTemplate.php
@@ -13,27 +13,27 @@
 
 <body>
     <?php wp_head(); ?>
-    
+
     <main class="mainContainer">
         <div class="hidden">
             <section class="contactsSection">
                 <!-- <div class="contact-box"> -->
-                    <form class="form-box">
-                        <h1>Contact me<span class="textColorChange">.</span></h1>
-                        <input type="text" class="input-field" placeholder="First name" name='firstname' autofocus>
-                        <input type="email" class="input-field" placeholder="Your Email" name='email' required>
-                        <input type="text" class="input-field" placeholder="Subject" name='subject'>
-                        <textarea type="text" class="input-field text-area" placeholder="Your message"
-                           name='message' required></textarea>
-                        <input type="submit" class="send-message-button" value="Send message">
-                    </form>
-                    <!-- </div> -->
+                <form class="form-box">
+                    <h1>Contact us<span class="textColorChange">.</span></h1>
+                    <input type="text" class="input-field" placeholder="First name" name='firstname' autofocus>
+                    <input type="email" class="input-field" placeholder="Your Email" name='email' required>
+                    <input type="text" class="input-field" placeholder="Subject" name='subject'>
+                    <textarea type="text" class="input-field text-area" placeholder="Your message" name='message'
+                        required></textarea>
+                    <input type="submit" class="send-message-button" value="Send message">
+                </form>
+                <!-- </div> -->
             </section>
         </div>
-    </main>   
-    
-    <?php wp_footer(); ?>   
+    </main>
 
+    <?php wp_footer(); ?>
+    
 </body>
 
-</html>     
+</html>
