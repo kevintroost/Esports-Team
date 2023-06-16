a9c40c6102e8599c4d33b73b881bcc969cf92e0a
PERENSAPP <119853517+PERENSAPP@users.noreply.github.com>
2023-06-16 09:56:54 +0200

    update

---
+++ wordpress/wp-content/themes/esportsTheme/contactTemplate.php
@@ -0,0 +1,39 @@
+<?php /* Template Name: contact page */?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php wp_nav_menu(array('theme_location' => 'header-menu')); ?>
+    <title></title>
+</head>
+
+<body>
+    <?php wp_head(); ?>
+    
+    <main>
+        <div class="hidden">
+            <section class="contactsSection">
+                <div class="contact-box">
+                    <form class="form-box">
+                        <h1>Contact me<span class="textColorChange">.</span></h1>
+                        <input type="text" class="input-field" placeholder="First name" name='firstname' autofocus>
+                        <input type="email" class="input-field" placeholder="Your Email" name='email' required>
+                        <input type="text" class="input-field" placeholder="Subject" name='subject'>
+                        <textarea type="text" class="input-field text-area" placeholder="Your message"
+                           name='message' required></textarea>
+                        <input type="submit" class="send-message-button" value="Send message">
+                    </form>
+                </div>
+            </section>
+        </div>
+    </main>   
+    
+    <?php wp_footer(); ?>   
+
+</body>
+
+</html>     
--- wordpress/wp-content/themes/esportsTheme/functions.php
+++ wordpress/wp-content/themes/esportsTheme/functions.php
@@ -1,21 +1,21 @@
-<?php 
+<?php
+require 'conn.php';
 // awdahfhaigh
 add_action("wp_enqueue_scripts", "loadCss");
-function loadCss(){ 
-   wp_enqueue_style("custom_css", get_template_directory_uri()."/style.css"); 
+function loadCss()
+{
+  wp_enqueue_style("custom_css", get_template_directory_uri() . "/style.css");
 }
-function register_my_menus() {
-    register_nav_menus(
-      array(
-        'header-menu' => __( 'Header Menu' ),
-        'extra-menu' => __( 'Extra Menu' )
-       )
-     );
-   }
-   add_action( 'init', 'register_my_menus' );
 
+function register_my_menus()
+{
+  register_nav_menus(
+    array(
+      'header-menu' => __('Header Menu'),
+      'extra-menu' => __('Extra Menu')
+    )
+  );
+}
+add_action('init', 'register_my_menus');
 
-
-   
-
-?> 
+?>
